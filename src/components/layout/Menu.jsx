285900453823416b2489eb7a4866c9147e0b8c79
M319209 <rafael.anacleto@SL094>
2023-10-17 08:23:55 -0300

    add font

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -2,7 +2,6 @@ import './Menu.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const Menu = props => (
 
     <aside className="Menu">
--- src/views/examples/About.jsx
+++ src/views/examples/About.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
 
 const About = props => (
 
     <div className='About'>
-     About
+
+        <section>
+            <h2>Nossa História</h2>
+            <p>
+                Insira aqui informações sobre a história da sua empresa, sua missão, visão e valores.
+            </p>
+        </section>
+
+        <section>
+            <h2>Nossa Equipe</h2>
+            <p>
+                Apresente informações sobre os membros da equipe ou funcionários da sua empresa.
+            </p>
+        </section>
+
+        <footer>
+            <p>&copy; 2023 Nome da Sua Empresa. Todos os direitos reservados.</p>
+        </footer>
     </div>
 )
 
